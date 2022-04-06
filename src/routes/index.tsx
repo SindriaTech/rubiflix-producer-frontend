a4bae539fd41058c9bea9b775f0a7b68345faf50
Pedro Troccoli <pedroaugustobelzkai@gmail.com>
2022-04-06 15:11:30 -0300

    fix: change name and update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Routes as DOMRoutes, Route } from 'react-router-dom';
 
 import { SignIn } from '../pages';
 
-export const routes: React.FC = () => (
+export const Routes: React.FC = () => (
   <BrowserRouter>
     <DOMRoutes>
+      <Route path="/" element={<SignIn />} />
       <Route path="/sign-in" element={<SignIn />} />
     </DOMRoutes>
   </BrowserRouter>
